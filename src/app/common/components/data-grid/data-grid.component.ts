a422d979ca02b6482de7551ff4c4970ec005559c
Ajinkya Chhatre <ajinkyachhatre@gmail.com>
2021-11-08 15:36:27 +0530

    05: Grid Component - single row select (#5)
    Changeset:
    1. Added a util function to find the closest ancestor
    2. Implemented selectRow method, takes care of selecting individual rows and toggle the state of checkbox to indeterminate true/false.

--- src/app/common/components/data-grid/data-grid.component.ts
+++ src/app/common/components/data-grid/data-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { findAncestor } from '../../utils';
 
 @Component({
   selector: 'app-data-grid',
@@ -40,6 +41,46 @@ export class DataGridComponent<T> {
     });
   }
 
+  selectRow(element: HTMLInputElement, row: T, index: number): void {
+    if (element.checked) {
+      // add the object to the selectedRows array
+      // the id will help to remove the entry since we do not want any coupling w.r.t the keys in the row object.
+      this.selectedRows.push({
+        id: index,
+        ...row
+      });
+    } else {
+      // remove the object from the selectedRows array
+      let indexToRemove = this.selectedRows.findIndex(item => item['id'] === index);
+      this.selectedRows.splice(indexToRemove, 1);
+    }
+
+    // set/unset the row color
+    const rowElement = findAncestor(element, 'row');
+    rowElement.setAttribute('className', element.checked ? 'row-selected' : '');
+
+    // set/unset indeterminate state of the selectAll checkbox
+    this.toggleCheckboxState();
+  }
+
   download(): void {
+  }
+
+  private toggleCheckboxState(): void {
+    if (this.selectedRows.length === this.data.length) {
+      // if all the checkboxes are checked
+      this.selectAllChkbox.nativeElement.indeterminate = false;
+      this.selectAllChkbox.nativeElement.className = '';
+      this.selectAllChkbox.nativeElement.checked = true;
+    } else if (this.selectedRows.length > 0) {
+      // if one or more checkbox(es) are checked
+      this.selectAllChkbox.nativeElement.indeterminate = true;
+      this.selectAllChkbox.nativeElement.className = 'indeterminate';
+    } else {
+      // if none are checked
+      this.selectAllChkbox.nativeElement.indeterminate = false;
+      this.selectAllChkbox.nativeElement.className = '';
+      this.selectAllChkbox.nativeElement.checked = false;
+    }
   }
 }
