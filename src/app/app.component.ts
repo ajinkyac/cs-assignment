ffb76615f337a27c3fd7afb071d5ac0481141fae
Ajinkya Chhatre <chhatrea@carbonblack.com>
2021-11-08 14:06:55 +0530

    02: Grid component (first cut)
    Changeset:
    1. Created a type interface to identify the data
    2. Create variables to hold the column names and data and pass them to the component
    3. Iterate and display column names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Process } from './common/types/type';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cs-assignment';
+  public columns: Array<String> = ['Name', 'Device', 'Path', 'Status'];
+  public processes: Array<Process> = [
+    {
+      name: 'smss.exe',
+      device: 'Stark',
+      path: '\\Device\\HarddiskVolume2\\Windows\\System32\\smss.exe',
+      status: 'scheduled'
+    },
+    {
+      name: 'netsh.exe',
+      device: 'Targaryen',
+      path: '\\Device\\HarddiskVolume2\\Windows\\System32\\netsh.exe',
+      status: 'available'
+    },
+    {
+      name: 'uxtheme.dll',
+      device: 'Lannister',
+      path: '\\Device\\HarddiskVolume1\\Windows\\System32\\uxtheme.dll',
+      status: 'available'
+    },
+    {
+      name: 'cryptbase.dll',
+      device: 'Martell',
+      path: '\\Device\\HarddiskVolume1\\Windows\\System32\\cryptbase.dll',
+      status: 'scheduled'
+    },
+    {
+      name: '7za.exe',
+      device: 'Baratheon',
+      path: '\\Device\\HarddiskVolume1\\temp\\7za.exe',
+      status: 'scheduled'
+    }
+  ];
 }
