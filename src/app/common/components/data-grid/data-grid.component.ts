b7d8b75e3bbf9fa1e36b77b8c6a8db310c1a5fb0
Ajinkya Chhatre <ajinkyachhatre@gmail.com>
2021-11-08 14:07:38 +0530

    02: Grid component (first cut) (#1)
    Changeset:
    1. Created a type interface to identify the data
    2. Create variables to hold the column names and data and pass them to the component
    3. Iterate and display column names
    
    Co-authored-by: Ajinkya Chhatre <chhatrea@carbonblack.com>

--- src/app/common/components/data-grid/data-grid.component.ts
+++ src/app/common/components/data-grid/data-grid.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-grid',
   templateUrl: './data-grid.component.html',
   styleUrls: ['./data-grid.component.scss']
 })
-export class DataGridComponent implements OnInit {
+export class DataGridComponent<T> implements OnInit {
+  @Input('columns') columns: Array<String>;
+  @Input('data') data: Array<T>;
 
   constructor() { }
 
---
+++ src/app/common/types/type.ts
@@ -0,0 +1,6 @@
+export interface Process {
+    name: string;
+    device: string;
+    path: string;
+    status: string;
+}
